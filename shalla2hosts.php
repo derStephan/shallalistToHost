6d48da3ce1fef0839cd78de2286a6f38eb191bc4
derStephan <derStephan@users.noreply.github.com>
2015-06-02 10:49:27 +0200

    Create shalla2hosts.php
    initial commit

---
+++ shalla2hosts.php
@@ -0,0 +1,213 @@
+
+<?php
+
+
+//set default list, so simply adding a parameter "download" will suffice
+$preSelection=array("ads","hacking","music","porn","sex","spyware","violence","adv","audio-video","tracker","warez","webradio","aggressive","drugs","podcasts","radiotv","updatesites","webtv","movies");
+if(!isset($_GET["selectedCategories"]))
+	$_GET["selectedCategories"]=$preSelection;
+
+$lastDownloadTime=getLastUpdateTime();
+
+//if local list is older than 1 week
+if($lastDownloadTime<strtotime("-1 week"))
+{
+	@unlink("shallalist.tar.gz");
+	@unlink("shallalist.tar");
+	downloadShallaList("http://www.shallalist.de/Downloads/shallalist.tar.gz");
+	unpackList();
+}
+
+//if downloadbutton is hit
+if(isset($_GET["download"]))
+{
+	//generate random file
+	$tempFileName="hosts".mt_rand();
+	
+	//add each selected category to hosts-file
+	foreach ($_GET["selectedCategories"] as $category)
+	{
+		assembleHostsFile($category,$tempFileName,$lastDownloadTime);
+	}
+	
+	//deliver it as download
+	header('Content-Disposition: attachment; filename="hosts"');
+	readfile($tempFileName);
+	
+	//delete temp file
+	unlink($tempFileName);
+}
+else
+{
+	//if downloadbutton is not pressed, show list.
+	showAvailableCategorySelection($lastDownloadTime);
+}
+
+
+function showAvailableCategorySelection($lastDownloadTime)
+{
+	//get main categories from list
+	$categoryDirectories=glob("list/BL/*");
+	//get already checked categories
+	$selectedCategories=$_GET["selectedCategories"];
+
+	?>
+	<!DOCTYPE html>
+	<head>
+		<title>Shalla to hosts</title>
+		<script type="text/javascript">
+		function toggle(source) 
+		{
+			checkboxes = document.getElementsByName('selectedCategories[]');
+			for(var i=0, n=checkboxes.length;i<n;i++) 
+			{
+				checkboxes[i].checked = source.checked;
+			}
+		}
+		</script>
+	
+	
+	</head>
+	<body>
+		
+	last list update: <?=date("c",$lastDownloadTime);?>
+	
+	<form method="GET">
+
+	
+	
+	<input type="checkbox" value="select all" onClick="toggle(this)" id="selectAll"><label for="selectAll">toggle all</label><br/><br />	
+		
+	<input type="submit" value="download hosts" name="download"><br />
+	<?php
+	foreach ($categoryDirectories as $categoryDirectory)
+	{
+		if(is_dir($categoryDirectory))
+		{
+			$category=explode("/",$categoryDirectory);
+			$category=$category[2];
+			?>
+			<input type="checkbox" name="selectedCategories[]" value="<?=$category?>" id="<?=$category?>" <?php if(in_array($category,@$selectedCategories)) echo "checked"; ?>><label for="<?=$category?>"><?=$category?></label><br />
+
+			<?php
+		}
+	}
+	?>
+	<input type="submit" value="download hosts" name="download">
+	</form>
+	</body>
+	</html>
+	<?php
+	
+}
+
+
+function getLastUpdateTime()
+{
+	if (!file_exists("lastDownload"))
+		return 0;
+	
+	$fp=fopen("lastDownload","r");
+	$lastDownloadTime=fgets($fp);
+	fclose($fp);
+	return $lastDownloadTime;
+}
+
+
+
+function downloadShallaList($url)
+{
+	// is cURL installed yet?
+	if (!function_exists('curl_init')){
+		die('Sorry cURL is not installed!');
+	}
+
+	// OK cool - then let's create a new cURL resource handle
+	$ch = curl_init();
+
+	// Now set some options (most are optional)
+
+	// Set URL to download
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+
+	// Include header in result? (0 = yes, 1 = no)
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	// Should cURL return or print out the data? (true = return, false = print)
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	// Timeout in seconds
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+	// Download the given URL, and save output
+	$contents = curl_exec($ch);
+
+	//write output to file
+	$fp=fopen("shallalist.tar.gz","w");
+	fwrite($fp, $contents);
+	fclose($fp);
+
+	// Close the cURL resource, and free system resources
+	curl_close($ch);
+	
+	//save downloadtime to file.
+	$fp=fopen("lastDownload","w");
+	fwrite($fp, time());
+	fclose($fp);
+	
+
+}
+
+function unpackList()
+{
+	$p = new PharData('shallalist.tar.gz');
+	$p->decompress(); // creates files.tar
+
+	// unarchive from the tar
+	$phar = new PharData('shallalist.tar');
+	$phar->extractTo('list',null,true); 
+}
+
+
+
+
+function assembleHostsFile($category,$hostsFilename,$lastDownloadTime)
+{
+	
+	$directory=glob("list/BL/$category");
+	
+	@$directory=$directory[0];
+	
+	if($directory[0]=="")
+		return false;
+	
+	if(file_exists ("$directory/domains"))
+		writeDomainsToHosts("$directory/domains",$hostsFilename,$lastDownloadTime);
+	else 
+	{
+		$subCategories=glob("$directory/*");
+		foreach($subCategories as $subCategory)
+		{
+			writeDomainsToHosts("$subCategory/domains",$hostsFilename,$lastDownloadTime);
+		}
+	}
+}
+
+function writeDomainsToHosts($filePath,$hostsFilename,$lastDownloadTime)
+{
+	$domains=fopen($filePath,"r");
+	$hosts=fopen($hostsFilename,"a");
+	
+	fwrite($hosts,"#last list update: ".date("c",$lastDownloadTime)."\n");
+	fwrite($hosts,"#domainlist: $filePath \n");
+	while($line = fgets($domains))
+	{
+		fwrite($hosts,"127.0.0.1 $line");
+	}
+		
+	fclose($hosts);
+	fclose($domains);
+}
+
+?>
